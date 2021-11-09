186585f387e09674806d7f1236f73bd961c3a57c
Thami Inaflas <inaflas.thami@gmail.com>
2021-11-09 20:28:16 +0100

    Added readme

---
+++ src/test/java/com/app/MaxMatchTest.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MaxMatchTest {
+    @Test
+    public void name() {
+        assertThat(true).isFalse();
+    }
+}
